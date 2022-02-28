c4cfd3cbf098e37dbc24743d493d96d65121eff1
Kamran Masood <kam1088@gmail.com>
2022-02-28 10:26:09 -0800

    completed pseudoclassical Grub.js class

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,19 @@
+// Properties:
+// age set to 0
+// color set to 'pink'
+// food set to 'jelly'
+
+// Methods:
+// eat
+
 var Grub = function() {
+
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+
 };
 
+Grub.prototype.eat = function() {
+
+};
