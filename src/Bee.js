8335b99724ecc951f1dc54c9415a6f02b188f57c
Kamran Masood <kam1088@gmail.com>
2022-02-28 10:26:42 -0800

    completed pseudoclassical Bee.js class

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,23 @@
+// call Grub superclass
+
+// Properties:
+// age set to 5
+// color set to 'yellow'
+// job set to 'keep on growing'
+
+// food (inherited)
+// eat (inherited)
+
+
 var Bee = function() {
+
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
